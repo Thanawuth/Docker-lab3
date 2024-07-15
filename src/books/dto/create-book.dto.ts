464a90511f965e7619af3cdb1bfc6d825667dbd2
Aftertnw <64160049@go.buu.ac.th>
2024-07-15 20:15:37 +0700

    books service

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,1 +1,26 @@
-export class CreateBookDto {}
+import {
+  IsString,
+  IsNumber,
+  IsBoolean,
+  IsNotEmpty,
+  Min,
+  Max,
+} from 'class-validator';
+
+export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+
+  @IsNumber()
+  @Min(1000)
+  @Max(new Date().getFullYear())
+  publishYear: number;
+
+  @IsBoolean()
+  isAvailable: boolean;
+}
