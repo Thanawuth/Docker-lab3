949d02c3287c732997dec999b30563ece0db1dca
Aftertnw <64160049@go.buu.ac.th>
2024-07-17 00:53:40 +0700

    process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,14 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ envFilePath: ['.env.local'], isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'rootpassword',
-      database: 'library_db',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Book],
       synchronize: true,
     }),
